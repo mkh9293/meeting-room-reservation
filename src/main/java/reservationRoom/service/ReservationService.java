15ec4053832b23e6f47fdf9fec9477001440981a
GwangHun Moon <mkh9293@gmail.com>
2019-11-13 00:02:48 +0900

    [Update] 주단위 반복 기능 까지  테스트 코드 추가

--- src/main/java/reservationRoom/service/ReservationService.java
+++ src/main/java/reservationRoom/service/ReservationService.java
@@ -1,17 +1,26 @@
 package reservationRoom.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import reservationRoom.dto.ReservationDto;
+import reservationRoom.entity.Reservation;
 import reservationRoom.repository.ReservationRepository;
+import reservationRoom.util.ReservationUtil;
 
 @Service
 public class ReservationService {
 	@Autowired
 	private ReservationRepository reservationRepository;
 	
-	public void reservation(ReservationDto reservation) {
-		reservationRepository.save(reservation.toEntity());
+	public boolean reservation(List<Reservation> list, Reservation reservation) {
+		if(ReservationUtil.checkDateTime(list, reservation)) {
+			reservationRepository.save(reservation);
+			return true;
+		}
+		
+		return false;
 	}
 }
--- src/test/java/reservationRoom/service/ReservationServiceTest.java
+++ src/test/java/reservationRoom/service/ReservationServiceTest.java
@@ -1,41 +1,144 @@
 package reservationRoom.service;
 
-import org.junit.Test;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import reservationRoom.entity.Reservation;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ReservationServiceTest {
+	
+	@Autowired
+	private ReservationService reservationService;
+	
+	private List<Reservation> reservations = new ArrayList<>();
+
+	
+	@Before
+	public void setUp() {
+		LocalDateTime startTime = LocalDateTime.of(LocalDate.of(2019, 11, 06), LocalTime.of(15, 0));
+		LocalDateTime endTime = startTime.plusHours(2); // 17:00
+		
+		reservations.add(Reservation.builder()
+				.roomName("A")
+				.userName("mkh")
+				.startDate(startTime)
+				.endDate(endTime)
+				.repeatCount(0)
+				.build());
+	}
+	
+	@Test
+	public void 정시또는30분단위가아닐때_일회성예약_테스트() {
+		//given
+		LocalDateTime startTime = LocalDateTime.of(LocalDate.of(2019, 11, 10), LocalTime.of(13, 20)); 
+		LocalDateTime endTime	= startTime.plusHours(3);
+		Reservation reservation = Reservation.builder()
+										.roomName("A")
+										.userName("mkh")
+										.startDate(startTime)
+										.endDate(endTime)
+										.repeatCount(0)
+										.build();
+				
+		//when
+		boolean result = reservationService.reservation(reservations, reservation);
+		
+		//then
+		assertFalse(result); 
+	}
+	
 	@Test
 	public void 중첩시간일때_일회성예약_테스트() {
 		//given
-		
+		LocalDateTime startTime = LocalDateTime.of(LocalDate.of(2019, 11, 06), LocalTime.of(13, 0)); 
+		LocalDateTime endTime	= startTime.plusHours(3);
+		Reservation reservation = Reservation.builder()
+										.roomName("A")
+										.userName("mkh")
+										.startDate(startTime)
+										.endDate(endTime)
+										.repeatCount(0)
+										.build();
+				
 		//when
+		boolean result = reservationService.reservation(reservations, reservation);
 		
 		//then
+		assertFalse(result); 
 	}
 	
 	@Test
 	public void 중첩시간아닐때_일회성예약_테스트() {
 		//given
-		
+		LocalDateTime startTime = LocalDateTime.of(LocalDate.of(2019, 11, 06), LocalTime.of(17, 0)); 
+		LocalDateTime endTime	= startTime.plusHours(3);
+		Reservation reservation = Reservation.builder()
+										.roomName("A")
+										.userName("mkh")
+										.startDate(startTime)
+										.endDate(endTime)
+										.repeatCount(0)
+										.build();
+				
 		//when
-		
+		boolean result = reservationService.reservation(reservations, reservation);
+
 		//then
-	}
-	
-	@Test
-	public void 중첩시간일때_주단위예약_테스트() {
-		//given
-		
-		//when
-		
-		//then
+		assertTrue(result); 
 	}
 	
 	@Test
 	public void 중첩시간아닐때_주단위예약_테스트() {
 		//given
-		
+		LocalDateTime startTime = LocalDateTime.of(LocalDate.of(2019, 11, 05), LocalTime.of(17, 0)); 
+		LocalDateTime endTime	= startTime.plusHours(3);
+		Reservation reservation = Reservation.builder()
+										.roomName("A")
+										.userName("mkh")
+										.startDate(startTime)
+										.endDate(endTime)
+										.repeatCount(5)
+										.build();
+				
 		//when
+		boolean result = reservationService.reservation(reservations, reservation);
 		
 		//then
+		assertTrue(result); 
+	}
+	
+	@Test
+	public void 중첩시간일때_주단위예약_테스트() {
+		//given
+		LocalDateTime startTime = LocalDateTime.of(LocalDate.of(2019, 11, 05), LocalTime.of(13, 0)); 
+		LocalDateTime endTime	= startTime.plusHours(3);
+		Reservation reservation = Reservation.builder()
+										.roomName("A")
+										.userName("mkh")
+										.startDate(startTime)
+										.endDate(endTime)
+										.repeatCount(2)
+										.build();
+				
+		//when
+		boolean result = reservationService.reservation(reservations, reservation);
+		
+		//then
+		assertFalse(result); 
 	}
 }
